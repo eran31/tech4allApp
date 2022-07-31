70258d9147070785bb3a548a92c2eefd5c08488a
chicool <eran@apptodate.co.il>
2022-07-31 11:01:20 +0300

    added todos.js

---
+++ src/Components/Todos.js
@@ -0,0 +1,13 @@
+function Todos() {
+
+    return (
+        <div>Todos
+        <button>hello</button>
+        <button>hello</button>
+        <button>hello</button>
+        <button>hello</button>
+        </div>
+    )
+};
+
+export default Todos;
